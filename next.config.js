d0b6b549cd13feac78788df939a5728aca8a41e7
Daniel Raphael <danielraphaelh@gmail.com>
2026-01-01 20:30:05 -0500

    Remove invalid Pages Router config from next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone', // For Docker deployment
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '50mb',
-    },
-  },
 }
 
 module.exports = nextConfig
